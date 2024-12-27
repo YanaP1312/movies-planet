be71b5e836b9d15a068725e7572650baf1e5a30d
Yana Pechenenko <pechenenko.ya@gmail.com>
2024-12-27 21:31:44 +0200

    add fetchfunctions for all requests

---
+++ src/tmdb-api.js
@@ -0,0 +1,33 @@
+import axios from "axios";
+
+const BASE_URL = "https://api.themoviedb.org/3";
+const API_KEY =
+  "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyZGE0ZDJiYjEwNGQwNTdjOWRlNjFjMzE2ODkzMDZlNiIsIm5iZiI6MTczNTMxNTM0MC41Miwic3ViIjoiNjc2ZWNmOGM2MmJhMzEwZmMwMTJkNTQ0Iiwic2NvcGVzIjpbImFwaV9yZWFkIl0sInZlcnNpb24iOjF9.zKNJtNvgYradpWH1OO4cyryMj8EuK2GDUPtZs-fOrpo";
+
+const axiosInstance = axios.create({
+  baseURL: BASE_URL,
+  headers: {
+    Authorization: `Bearer ${API_KEY}`,
+  },
+});
+
+export async function fetchTrendingMovies() {
+  const response = await axiosInstance.get("/trending/movie/day");
+  return response.data;
+}
+
+export async function fetchSearchMovies(userQuery) {
+  const response = await axiosInstance.get("/search/movie", {
+    params: { query: userQuery },
+  });
+  return response.data;
+}
+
+export async function fetchMoviesDetails(id, appendResponse = "") {
+  const response = await axiosInstance.get(`/movie/${id}`, {
+    params: {
+      append_to_response: appendResponse,
+    },
+  });
+  return response.data;
+}
